9eb6098143ea733678913623efcfb5cd57a9b30c
Sacamano604 <bentoussi@gmail.com>
2016-03-09 18:33:57 -0800

    Linking Firebase

--- js/addresseyControllers.js
+++ js/addresseyControllers.js
@@ -3,6 +3,9 @@
 // Addressey Controller Module
 var addresseyControllers = angular.module('addresseyControllers', []);
 
+
+
+
 // Controller that handles which nav link is active based on the current page loaded
 addresseyControllers.controller('navController', ["$scope", "$location",
   function($scope, $location) {
